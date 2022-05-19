f0bf290c29a8cb7dbdf31a4293b5c1eac9784f67
tuan2904 <dtuan290400@gmail.com>
2022-05-19 19:49:01 +0700

    l

--- _s2_design_createcsdl/bai_tap/bai_tap_quan_ly_vat_tu.sql
+++ _s2_design_createcsdl/bai_tap/bai_tap_quan_ly_vat_tu.sql
@@ -1,56 +1,56 @@
 create database quan_ly_vat_tu;
 use quan_ly_vat_tu;
 create table phieu_xuat(
-so_px int primary key,
+so_phieu_xuat int primary key,
 ngay_xuat date
 );
 create table vat_tu(
-ma_vt int primary key,
-ten_vt varchar(50)
+ma_vat_tu int primary key,
+ten_vat_tu varchar(50)
  );
  create table chi_tiet_phieu_xuat(
-so_px int,
+so_phieu_xuat int,
 ma_vat_tu int,
 don_gia_xuat double,
 so_luong_xuat int,
-primary key(so_px,ma_vat_tu),
-foreign key (so_px)references phieu_xuat(so_px),
-foreign key(ma_vat_tu)references vat_tu(ma_vt)
+primary key(so_phieu_xuat,ma_vat_tu),
+foreign key (so_phieu_xuat)references phieu_xuat(so_phieu_xuat),
+foreign key(ma_vat_tu)references vat_tu(ma_vat_tu)
 );
  create table phieu_nhap(
- so_pn int primary key,
+ so_phieu_nhap int primary key,
  ngay_nhap date
  );
  create table chi_tiet_phieu_nhap(
 ma_vat_tu int,
-so_pn int,
+so_phieu_nhap int,
 don_gia_nhap double,
 so_luong_nhap int,
-primary key (so_pn,ma_vat_tu),
-foreign key (so_pn)references phieu_nhap(so_pn),
-foreign key(ma_vat_tu)references vat_tu(ma_vt)
+primary key (so_phieu_nhap,ma_vat_tu),
+foreign key (so_phieu_nhap)references phieu_nhap(so_phieu_nhap),
+foreign key(ma_vat_tu)references vat_tu(ma_vat_tu)
  );
  create table nha_cung_cap (
- ma_ncc int primary key,
- ten_ncc varchar(50),
+ ma_nha_cung_cap int primary key,
+ ten_nha_cung_cap varchar(50),
  dia_chi varchar(50)
  );
-  create table don_dh(
- so_dh int,
- ngay_dh date,
- ma_ncc int,
- primary key(so_dh),
- foreign key (ma_ncc) references nha_cung_cap(ma_ncc)
+  create table don_dat_hang(
+ so_don_hang int,
+ ngay_don_hang date,
+ ma_nha_cung_cap int,
+ primary key(so_don_hang),
+ foreign key (ma_nha_cung_cap) references nha_cung_cap(ma_nha_cung_cap)
  );
 create table chi_tiet_don_hang(
-ma_vt int,
-so_dh int,
-primary key (ma_vt,so_dh),
-foreign key(ma_vt)references vat_tu(ma_vt),
-foreign key(so_dh)references don_dh(so_dh)
+ma_vat_tu int,
+so_don_hang int,
+primary key (ma_vat_tu,so_don_hang),
+foreign key(ma_vat_tu)references vat_tu(ma_vat_tu),
+foreign key(so_don_hang)references don_dat_hang(so_don_hang)
 );
 create table phone(
 ma_nha_cung_cap int,
 phone_number char(20) primary key,
-foreign key(ma_nha_cung_cap)references nha_cung_cap(ma_ncc)
+foreign key(ma_nha_cung_cap)references nha_cung_cap(ma_nha_cung_cap)
 );
